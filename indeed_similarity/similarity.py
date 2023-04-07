51737a57a15e96bcee389ad13d30f44bfab83b84
Kritkorn Supyen <77403352+mike28270@users.noreply.github.com>
2023-04-07 12:06:33 +0200

    from support df to either list or numpy array

--- indeed_similarity/similarity.py
+++ indeed_similarity/similarity.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Union
 
 import spacy
 import numpy as np
@@ -13,68 +13,74 @@ from sentence_transformers import SentenceTransformer, util
 import warnings
 warnings.filterwarnings("ignore", message=r"\[W008\]", category=UserWarning)
 
-class Pipeline:
+class SimilarityPipeline:
+    """
+    A pipeline for multiple similarity functions
+
+    Args:
+        - similarity_functions: A list of similarity funtions based on BaseSimilarity Class
+    """
     def __init__(self, similarity_functions:List) -> None:
         self.similarity_functions = similarity_functions
 
     def __len__(self):
         return len(self.similarity_functions)
 
-    def __call__(self, df1:pd.DataFrame, df2:pd.DataFrame, param:Tuple):
+    def __call__(self, a:Union[np.array, List], b:Union[np.array, List]):
         results = {}
         for func in self.similarity_functions:
-            results[func.__name__] = func(df1, df2, param)()
+            results[func.__name__] = func(a, b)
         return results
-        
 
-class BaseSimilarity:
-    def __init__(self, df1:pd.DataFrame, df2:pd.DataFrame) -> None:
-        self.entity_list, self.entity_list2 = df1["name"].values, df2["name"].values
-        self.label_list, self.label_list2 = df1["label"].values, df2["label"].values
-        self.l1, self.l2 = df1["path"].values, df2["path"].values
 
-    # TODO: param from the old function do nothing. Need to make a filter to filter out the similarity score.
-    def similarity_matrix(self, similarity_func:Any, param:Tuple) -> pd.DataFrame:
-        #Create empty matrix to fill
-        M_sim = np.zeros((self.l1.shape[0], self.l2.shape[0]))
+class BaseSimilarity:    
+    """
+    Find the similarity between 2 lists of string
 
-        #Iterate and fill 
-        for i in range(self.l1.shape[0]):
-            u = self.label_list[i]
-            for j in range(self.l2.shape[0]):
-                v = self.label_list2[j]
-                #similarity -> structural similarity 
-                M_sim[i, j] = similarity_func(u, v)
-        self.df_sim = pd.DataFrame(M_sim, columns=self.entity_list2, index=self.entity_list)
+    Args:
+        - a: the first list of string
+        - b: the second list of string
+    """
+    def __init__(self, a:Union[np.array, List], b:Union[np.array, List]) -> None:
+        self.checkInputType(a)
+        self.checkInputType(b)
+        self.a = a
+        self.b = b
+
+    @staticmethod
+    def checkInputType(value):
+        assert isinstance(value, (list, np.array)), "The input must be either a list or numpy array"
+        assert len(value) > 0 , "The list or numpy array is empthy"
+
+    def preprocess(self, a, b):
+        return a, b
+
+    def similarity_matrix(self, similarity_func:Any) -> pd.DataFrame:
+        a, b = self.preprocess(self.a, self.b)
+        arr_sim = np.zeros((len(a), len(b)))  # Create empty matrix to fill
+
+        for i, text1 in enumerate(a):
+            for j, text2 in enumerate(b):
+                arr_sim[i, j] = similarity_func(text1, text2)
+        self.df_sim = pd.DataFrame(arr_sim, columns=self.b, index=self.a)
         return self.df_sim
     
-    def plot(self, threshold):
+    def plot(self, threshold:int=1):
         assert self.df_sim is not None, "The value of similarity matrix is None"
-        plot_df = self.df_sim.copy()
-        plot_df[plot_df < threshold] = 0.0
-        rows, cols = self.df_sim.shape[0:1]
-
-        plt.figure(figsize=((0.4*rows),(0.4*cols)))
-        cmap = sns.diverging_palette(230, 20, as_cmap=True)
-        return sns.heatmap(self.df_sim, fmt="g", cmap=cmap,linewidths=0.5, linecolor='black')
+        df_plot = self.df_sim[self.df_sim<=threshold].fillna(0)
+        return sns.heatmap(df_plot)
 
 
 class LevenshteinSimilarity(BaseSimilarity):
-    def __init__(self, df1:pd.DataFrame, df2:pd.DataFrame, param:Tuple) -> None:
-        super().__init__(df1, df2)
-        self.param = param
+    def __init__(self, a:Union[np.array, List], b:Union[np.array, List]) -> None:
+        super().__init__(a, b)
+        self.similarity_matrix(LevenshteinRatio)
 
-    def __call__(self, *args: Any, **kwds: Any) -> pd.DataFrame:
-        return self.similarity_matrix(LevenshteinRatio, self.param)
-    
 
 class JaccardSimilarity(BaseSimilarity):
-    def __init__(self, df1:pd.DataFrame, df2:pd.DataFrame, param:Tuple) -> None:
-        super().__init__(df1, df2)
-        self.param = param
-
-    def __call__(self, *args: Any, **kwds: Any) -> pd.DataFrame:
-        return self.similarity_matrix(self.jaccard_similarity, self.param)
+    def __init__(self, a:Union[np.array, List], b:Union[np.array, List]) -> None:
+        super().__init__(a, b)
+        self.similarity_matrix(self.jaccard_similarity)
 
     def jaccard_similarity(self, x, y):
         """ returns the jaccard similarity between two lists """
@@ -84,46 +90,42 @@ class JaccardSimilarity(BaseSimilarity):
 
 
 class SequenceSimilarity(BaseSimilarity):
-    def __init__(self, df1:pd.DataFrame, df2:pd.DataFrame, param:Tuple) -> None:
-        super().__init__(df1, df2)
-        self.param = param
-
-    def __call__(self, *args: Any, **kwds: Any) -> pd.DataFrame:
-        return self.similarity_matrix(self.sequence_similarity, self.param)
+    def __init__(self, a:Union[np.array, List], b:Union[np.array, List]) -> None:
+        super().__init__(a, b)
+        self.similarity_matrix(self.sequence_similarity)
 
     def sequence_similarity(self, x, y):
         return SequenceMatcher(None, x, y).ratio()
 
 
 class BertTransformerSimilarity(BaseSimilarity):
-    def __init__(self, df1:pd.DataFrame, df2:pd.DataFrame, param:Tuple, model_name:str="all-MiniLM-L6-v2") -> None:
-        super().__init__(df1, df2)
-        self.param = param
+    def __init__(self, a:Union[np.array, List], b:Union[np.array, List], model_name:str="all-MiniLM-L6-v2") -> None:
+        super().__init__(a, b)
         self.model = SentenceTransformer(model_name)
-
-    def __call__(self, *args: Any, **kwds: Any) -> pd.DataFrame:
-        return self.similarity_matrix(self.transformers_similarity, self.param)
+        self.similarity_matrix(self.transformers_similarity)
     
+    def preprocess(self, a, b):
+        doc_function = lambda text: self.model.encode(str(text), convert_to_tensor=True)
+        a = list(map(doc_function, a))
+        b = list(map(doc_function, b))
+        return a, b
+
     def transformers_similarity(self, x, y):
-        embedding1 = self.model.encode(x, convert_to_tensor=True)
-        embedding2 = self.model.encode(y, convert_to_tensor=True)
-        # compute similarity scores of two embeddings
-        cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
+        cosine_scores = util.pytorch_cos_sim(x, y)
         return cosine_scores.item()
 
 
 class SpacyTransformerSimilarity(BaseSimilarity):
-    def __init__(self, df1:pd.DataFrame, df2:pd.DataFrame, param:Tuple, model_name:str="en_core_web_md") -> None:
-        super().__init__(df1, df2)
-        self.param = param
+    def __init__(self, a:Union[np.array, List], b:Union[np.array, List], model_name:str="en_core_web_md") -> None:
+        super().__init__(a, b)
         self.model = spacy.load(model_name)
+        self.similarity_matrix(self.transformers_similarity)
 
-        self.doc_function = np.vectorize(self.model)
-        self.label_list = self.doc_function(df1["label"].values)
-        self.label_list2 = self.doc_function(df2["label"].values)
+    def preprocess(self, a, b):
+        doc_function = lambda text: self.model(str(text))
+        a = list(map(doc_function, a))
+        b = list(map(doc_function, b))
+        return a, b
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        return self.similarity_matrix(self.transformers_similarity, self.param)
- 
     def transformers_similarity(self, x, y):
         return x.similarity(y)
